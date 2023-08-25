fe89d360f24059fad914bb07369ab63627f5cce3
crzypatchwork <costabraba93@gmail.com>
2023-08-24 22:00:48 -0300

    bidiretional swap

--- src/components/defi.jsx
+++ src/components/defi.jsx
@@ -2,9 +2,9 @@ import React, { Component, useContext, useState } from 'react'
 import { UngrundContext } from '../context/UngrundContext'
 import {
     useContractRead,
-    useContractWrite, 
+    useContractWrite,
     useWaitForTransaction,
-    usePrepareContractWrite, 
+    usePrepareContractWrite,
 } from 'wagmi'
 import { parseEther } from 'viem'
 import { useDebounce } from 'usehooks-ts'
@@ -26,66 +26,93 @@ const getAllowance = () => {
 
 const Approve = (account, amount) => {
     const { wuwei, wuweiAbi, lp } = useContext(UngrundContext)
+
     const { config } = usePrepareContractWrite({
         address: wuwei,
         abi: wuweiAbi,
         functionName: 'Approve',
         args: [account, lp, amount],
     })
+
     const { data, write } = useContractWrite(config)
+
     const { isLoading, isSuccess } = useWaitForTransaction({
-    hash: data?.hash,
+        hash: data?.hash,
     })
-    const onSubmit = () => {
-        write?.()
-    }
-    return(
+
+    const onSubmit = () => write?.()
+
+    return (
         <div><br />
             {
                 <div>
-                    <a className="button style" onClick={() => onSubmit() } style={{ cursor: 'pointer' }}>approve for swap</a>
+                    <a className="button style" onClick={() => onSubmit()} style={{ cursor: 'pointer' }}>approve for swap</a>
                 </div>
             }
         </div>
     )
 }
 
-const Transaction = () => {
-    const { lp, lpAbi, account } = useContext(UngrundContext)
-    const [dx, setDx] = useState(0)
-    const [dy, setDy] = useState(0)
-    const [amount, setAmount] = useState(0)
-    const [minDy, setminDy] = useState(0)
-    const [swapI, setSwapI] = useState(1)
-    const [swapJ, setSwapJ] = useState(0)
-    
-    const debouncedDx = useDebounce(dx, 500)
-    const debouncedMinDy = useDebounce(dy, 500)
-
-    
-    const { config } = usePrepareContractWrite({
+const GetDy = props => {
+    const { lp, lpAbi } = useContext(UngrundContext)
+    const { data, isError, isLoading } = useContractRead({
         address: lp,
         abi: lpAbi,
-        chainId: polygon.id,
-        functionName: 'exchange_underlying',
-        stateMutability: 'payable',
-        value: parseEther((dx * (10 ** 18)).toString()),
-        args: [parseInt(swapI), parseInt(swapJ), parseEther((dx * (10 ** 18)).toString()), parseEther(debouncedMinDy.toString()), account],
-        //enabled: [Boolean(swapI), Boolean(swapJ), Boolean(dx), Boolean(minDy)]
+        functionName: 'get_dy',
+        args: props._directional ? [1, 0, parseEther(props._dx.toString())] : [0, 1, parseEther(props._dx.toString())],
     })
+    return (
+        <div>
+            <input type="text" placeholder="0" id="dy" value={data ? parseInt(BigInt(data)) / 10 ** 18 : 0} />
+            {!props._directional ? <span>MATIC</span> : <span>███</span>}
+            <br />
+        </div>
+    )
+}
+
+const Exchange = props => {
+    const { lp, lpAbi, account } = useContext(UngrundContext)
+    let config = {}
+    props._directional ?
+        config = {
+            address: lp,
+            abi: lpAbi,
+            chainId: polygon.id,
+            functionName: 'exchange_underlying',
+            stateMutability: 'payable',
+            value: parseEther(props._dx.toString()),
+            args: [1, 0, parseEther(props._dx.toString()), parseEther("0"), account]    
+        } :
+        config = {
+            address: lp,
+            abi: lpAbi,
+            chainId: polygon.id,
+            functionName: 'exchange_underlying',
+            args: [0, 1, parseEther(props._dx.toString()), parseEther("0"), account],
+        }
 
     const { data, write } = useContractWrite(config)
+
     const { isLoading, isSuccess } = useWaitForTransaction({
         hash: data?.hash,
-    }) 
+    })
+
+    const onSubmit = () => write?.()
 
     return (
         <div><br />
             {
                 <div>
-                    <input type="text" placeholder="amount" id="dx" value={dx} onChange={(e) => setDx(e.target.value)} /><br />
-    
-                    <a className="button style" onClick={() => write?.() } style={{ cursor: 'pointer' }}>swap</a>
+                    {/*                     <input type="text" placeholder="0" id="dx" value={dx} onChange={(e) => {
+                        setDirectional(true)
+                        setDx(e.target.value)
+                        }} /><br />
+                    <a className="button style" style={{ cursor : 'pointer'}}>⇅</a><br/>
+                    <input type="text" placeholder="0" id="dy" value={dy} onChange={e => {
+                        setDirectional(false)
+                        setDy(e.target.value)
+                        }} /><br /> */}
+                    <a className="button style" onClick={() => onSubmit()} style={{ cursor: 'pointer' }}>convert</a>
                 </div>
             }
         </div>
@@ -93,16 +120,29 @@ const Transaction = () => {
 }
 
 export const Defi = () => {
+    const [directional, setDirectional] = useState(true)
+    const [dx, setDx] = useState(0)
 
-    let allowance = getAllowance()
-    console.log(allowance)
+    // review allowances
+    
+    //let allowance = getAllowance()
+    //console.log(allowance)
 
     //if (!allowance) return <Approve  account={account} amount={dx} />
     //else return <Transaction tokenId={ tokenId } />
     return (
         <div>
-            oi
-            <Transaction />
+            <div>
+                <br />
+                <input type="text" placeholder="0" id="dx" value={dx} onChange={(e) => { setDx(e.target.value) }} />
+                {directional ? <span>MATIC</span> : <span>███</span>}
+                <br />
+                <a className="button style" style={{ cursor: 'pointer' }} onClick={() => setDirectional(!directional)}>⇅</a>
+                <br />
+                <GetDy _directional={directional} _dx={dx} />
+                {/* slippage warning */}
+            </div>
+            <Exchange _directional={directional} _dx={dx} />
         </div>
     )
 }
